e909a90a4a9dce651973b8da75bdcfe426dbad61
pavankattamuri8 <147482466+pavankattamuri8@users.noreply.github.com>
2026-05-20 19:02:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello CI/CD Pipeline Working ');
+    res.send('hello ');
 });
 
 app.listen(3000, () => {
